22ad33e2068aeb75c97150e376f1f7a43b654f94
Jason Gong <mhpjay@gmail.com>
2018-11-13 17:41:22 -0500

    update

---
+++ app/models/user.rb
@@ -0,0 +1,68 @@
+class User < ApplilcationRecord
+
+validates :username, :email, :password_digest, :session_token, presence: true
+validates :username, :email, uniqueness: true
+validates :password, length: { minimum: 6 }, allow_nil: true
+
+after_initialize :ensure_session_token
+
+has_many :listings
+has_many :reviews
+
+attr_reader :password
+
+def self.find_by_credentials(username, password)
+  user = User.find_by(username: username)
+  return nil unless user
+  user.is_password?(password) ? user : nil
+end
+
+def password=(password)
+  @password = password
+  self.password_digest = BCrypt::Password.create(password)
+end
+
+def is_password(password)
+  BCrypt::Password.new(self.password_digest).is_password?(password)
+end
+
+def reset_session_token!
+  generate_session_token
+  save!
+  self.session_token
+end
+
+private
+
+def ensure_session_token
+  generate_session_token unless self.session_token
+end
+
+def new_session_token
+  SecureRandom.urlsafe_base64
+end
+
+def generate_session_token
+  self.session_token = new_session_token
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ frontend/meepleBnB.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<h1>Welcome to MeepleBnB</h1>, document.getElementById('root'));
+});
