21a8a323da2e14438335fb5626032fafc7422b12
Jason Gong <mhpjay@gmail.com>
2018-11-20 00:51:00 -0500

    Added styling for Show page pictures and body

--- frontend/components/listing_show/listing_detail.jsx
+++ frontend/components/listing_show/listing_detail.jsx
@@ -6,18 +6,30 @@ const ListingDetail = ({ listing }) => {
   return (
     <div>
       <NavbarContainer/>
-      <div className="sidefill">
-      </div>
-      <ul className="listing-list">
-        <img className="index-image" src={listing.picture_url}/>
-        <li>Title: {listing.title}</li>
-        <li>Description: {listing.description}</li>
-        <li>Address: {listing.address}</li>
-        <li>Price: {listing.price}</li>
-        <li>Latitude: {listing.lat}</li>
-        <li>Longitude: {listing.lng}</li>
-      </ul>
-      <div className="sidefill">
+      <div className="list-show-body">
+
+        <div className="list-show-images">
+          <div className="list-show-image-main">
+            <img className="index-image-main-1" src={listing.picture_url}/>
+          </div>
+          <div className="list-show-image-side">
+            <img className="index-image" src={listing.picture_url}/>
+            <img className="index-image" src={listing.picture_url}/>
+            <img className="index-image" src={listing.picture_url}/>
+            <img className="index-image" src={listing.picture_url}/>
+          </div>
+
+        </div>
+        <ul className="listing-list">
+          <li>Title: {listing.title}</li>
+          <li>Description: {listing.description}</li>
+          <li>Address: {listing.address}</li>
+          <li>Price: {listing.price}</li>
+          <li>Latitude: {listing.lat}</li>
+          <li>Longitude: {listing.lng}</li>
+        </ul>
+        <form className="booking-form">
+        </form>
       </div>
     </div>
   );
