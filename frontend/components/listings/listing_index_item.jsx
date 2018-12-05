2b9c89d242df8ebf0f40f0f23a285f8d23b87c1c
Jason Gong <mhpjay@gmail.com>
2018-12-05 10:50:02 -0500

    Added favicon and change cursor back to default if img is not clickable

--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -15,31 +15,33 @@ class IndexItem extends React.Component {
 
   render() {
     return (
-      <div className="listing-index-item" onClick={this.handleClick}>
-        <div className="index-item-img">
-          <img className="list-img" src={this.props.listing.picture_url} />
+      <div className="listing-index">
+        <div className="listing-index-item" onClick={this.handleClick}>
+          <div className="index-item-img">
+            <img className="list-img" src={this.props.listing.picture_url} />
+          </div>
+          <div className="index-item-info">
+            <img
+              className="plus"
+              src="./Screen Shot 2018-11-23 at 11.15.19 AM.png"
+            />
+            <span className="index-item-title">{this.props.listing.title}</span>
+            <br />
+            <span className="index-item-price">
+              ${this.props.listing.price} per night · Free cancellation
+            </span>
+            <br />
+            <br />
         </div>
-        <div className="index-item-info">
-          <img
-            className="plus"
-            src="./Screen Shot 2018-11-23 at 11.15.19 AM.png"
-          />
-          <span className="index-item-title">{this.props.listing.title}</span>
-          <br />
-          <span className="index-item-price">
-            ${this.props.listing.price} per night · Free cancellation
-          </span>
-          <br />
-          <span className="index-item-rating">
-            <div>Rating: </div>
-            <img className="star" src="./star-solid.svg" />
-            <img className="star" src="./star-solid.svg" />
-            <img className="star" src="./star-solid.svg" />
-            <img className="star" src="./star-solid.svg" />
-            <img className="star" src="./star-solid.svg" />
-          </span>
-          <br />
-        </div>
+      </div>
+      <span className="index-item-rating">
+      <div>Rating: </div>
+      <img className="star" src="./star-solid.svg" />
+      <img className="star" src="./star-solid.svg" />
+      <img className="star" src="./star-solid.svg" />
+      <img className="star" src="./star-solid.svg" />
+      <img className="star" src="./star-solid.svg" />
+      </span>
       </div>
     );
   }
