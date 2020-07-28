ce88e4a419d69d46320a682546e71bc9b612c206
Jason Gong <mhpjay@gmail.com>
2020-07-28 14:56:24 -0400

    edit list show header

--- frontend/components/listing_show/listing_detail.jsx
+++ frontend/components/listing_show/listing_detail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 import NavbarContainer from "../navbar/navbar_container";
 import BookingFormContainer from "./booking_form_container";
+import Rating from "react-rating";
 
 
 class ListingDetail extends React.Component {
@@ -39,33 +40,54 @@ class ListingDetail extends React.Component {
     <div className="listing-detail-page">
       <NavbarContainer />
       <div className="list-show-body">
-        <div className="list-show-images">
-          <div className="list-show-image-main">
-            <img className="index-image-main-1" src={this.props.listing.picture_url} />
+        <div className="list-show-header">
+          <div className="list-show-header-description-container">
+            <div className="list-show-header-description">
+              <div className="list-show-header-description-text">
+                {this.props.listing.title}
+              </div>
+              <div className="list-show-header-description-otherInfo">
+                <Rating
+                  className="show-rating"
+                  placeholderRating={1}
+                  emptySymbol={<img src="./star-solid.svg" className="star" />}
+                  placeholderSymbol={<img src="./star-solid.svg" className="star" />}
+                  fullSymbol={<img src="./star-solid.svg" className="star" />}
+                />
+                <span className="show-interpunct"> · </span>
+                <div className="list-show-header-location">
+                  New York, United States
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="list-show-image-side">
-            <img className="index-image" src={"3-1200x800.jpg"} />
-            <img
-              className="index-image"
-              src={"Black-looks-exquisite-in-the-bachelor-pad-bedroom.jpg"}
-            />
-            <img
-              className="index-image"
-              src={"pharrell-williams-miami-penthouse-kitchen-1.jpg"}
-            />
-            <img
-              className="index-image"
-              src={
-                "luxury-pictures-of-modern-bathrooms-21-ideas-beautiful-bathroom-designs-from-schmidt.jpg"
-              }
-            />
+          <div className="list-show-images">
+            <div className="list-show-image-main">
+              <img className="index-image-main-1" src={this.props.listing.picture_url} />
+            </div>
+            <div className="list-show-image-side">
+              <img className="index-image" src={"3-1200x800.jpg"} />
+              <img
+                className="index-image"
+                src={"Black-looks-exquisite-in-the-bachelor-pad-bedroom.jpg"}
+              />
+              <img
+                className="index-image"
+                src={"pharrell-williams-miami-penthouse-kitchen-1.jpg"}
+              />
+              <img
+                className="index-image"
+                src={
+                  "luxury-pictures-of-modern-bathrooms-21-ideas-beautiful-bathroom-designs-from-schmidt.jpg"
+                }
+              />
+            </div>
           </div>
         </div>
-        <ul className="listing-body">
+        <div className="listing-body">
           <div className="listing-lis">
             <div className="property-type">ENTIRE HOUSE</div>
             <div className="listing-title">
-              <li>{this.props.listing.title}</li>
             </div>
             <div className="listing-amenities-preview">
               <div className="preview-house">
@@ -110,21 +132,19 @@ class ListingDetail extends React.Component {
             </div>
             <span className="the-space">The space</span>
             <div>{this.props.listing.description}</div>
-            <li />
-            <li />
+
             <li>Address: {this.props.listing.address}</li>
             <div className="lat-lng">
               {this.props.listing.lat}
               {this.props.listing.lng}
             </div>
             <div className="street-map" ref={map => (this.mapNode = map)}></div>
-            <li />
-            <li />
+
             
             
           </div>
           <BookingFormContainer listing={this.props.listing} />
-        </ul>
+        </div>
       </div>
     </div>
   );
