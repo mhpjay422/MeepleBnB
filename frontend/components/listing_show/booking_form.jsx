af847c7eaff739a50fdfb392be755253301053f1
Jason Gong <mhpjay@gmail.com>
2020-07-29 21:22:07 -0400

    overide date picker API JS styling

--- frontend/components/listing_show/booking_form.jsx
+++ frontend/components/listing_show/booking_form.jsx
@@ -87,58 +87,61 @@ class BookingForm extends React.Component {
       <div className="booking-form">
         <div className="booking-frame">
           <div className="booking-body">
-            <div className="booking-body-margin">
-              <div className="booking-price">
-                <div className="booking-price-integer">
-                  ${this.props.listing.price}
+            <div className="booking-padding">
+              <div className="booking-body-margin">
+                <div className="booking-price">
+                  <div className="booking-price-integer">
+                    ${this.props.listing.price}
+                  </div>
+                  <div className="booking-price-pernight">
+                    <div>per night</div>
+                  </div>
                 </div>
-                <div className="booking-price-pernight">
-                  <div>per night</div>
+                <div className="booking-rating">
+                  <div className="booking-rating-text">Rating: </div>
+                  <Rating
+                    placeholderRating={3.5}
+                    emptySymbol={<img src="./star-solid.svg" className="star" />}
+                    placeholderSymbol={<img src="./star-solid.svg" className="star" />}
+                    fullSymbol={<img src="./star-solid.svg" className="star" />}
+                  />
                 </div>
-              </div>
-              <div className="booking-rating">
-                <div className="booking-rating-text">Rating: </div>
-                <Rating
-                  placeholderRating={3.5}
-                  emptySymbol={<img src="./star-solid.svg" className="star" />}
-                  placeholderSymbol={<img src="./star-solid.svg" className="star" />}
-                  fullSymbol={<img src="./star-solid.svg" className="star" />}
+                <div className="booking-dates-header">Dates</div>
+                <div className="date-picker">
+                  <DateRangePicker
+                    className="date-range-picker-api"
+                    startDate={this.state.startDate}
+                    startDateId="start-date"
+                    endDate={this.state.endDate}
+                    endDateId="end-date"
+                    startDatePlaceholderText="Check In"
+                    showClearDates={true}
+                    endDatePlaceholderText="Check Out"
+                    onDatesChange={({ startDate, endDate }) =>
+                      this.setState({ startDate, endDate })
+                    }
+                    focusedInput={this.state.focusedInput}
+                    onFocusChange={focusedInput => this.setState({ focusedInput })}
+                    renderCalendarDay={undefined}
+                  />
+                </div>
+                <div className="booking-guests-header">Guests</div>
+                <input
+                  type="integer"
+                  className="booking-guests-input"
+                  defaultValue="1"
+                  min="1"
+                  onChange={this.handleGuests}
                 />
+                <div>{this.renderErrors()}</div>
+                <div className="button-hover">
+                  <button className="booking-submit" onClick={this.handleSubmit}>
+                    <div className="book-button">Book</div>
+                  </button>
+                </div>
+                <div className="booking-charge-text">You won’t be charged yet</div>
+                <div className="booking-divider" />
               </div>
-              <div className="booking-dates-header">Dates</div>
-              <div className="date-picker">
-                <DateRangePicker
-                  startDate={this.state.startDate}
-                  startDateId="start-date"
-                  endDate={this.state.endDate}
-                  endDateId="end-date"
-                  startDatePlaceholderText="Check In"
-                  showClearDates={true}
-                  endDatePlaceholderText="Check Out"
-                  onDatesChange={({ startDate, endDate }) =>
-                    this.setState({ startDate, endDate })
-                  }
-                  focusedInput={this.state.focusedInput}
-                  onFocusChange={focusedInput => this.setState({ focusedInput })}
-                  renderCalendarDay={undefined}
-                />
-              </div>
-              <div className="booking-guests-header">Guests</div>
-              <input
-                type="integer"
-                className="booking-guests-input"
-                defaultValue="1"
-                min="1"
-                onChange={this.handleGuests}
-              />
-              <div>{this.renderErrors()}</div>
-              <div className="button-hover">
-                <button className="booking-submit" onClick={this.handleSubmit}>
-                  <div className="book-button">Book</div>
-                </button>
-              </div>
-              <div className="booking-charge-text">You won’t be charged yet</div>
-              <div className="booking-divider" />
             </div>
           </div>
           <div className="booking-bottom-container">
