60f8638ebd44b8ac8105c4679350fc37e3e12414
Jason Gong <mhpjay@gmail.com>
2020-08-02 15:27:08 -0400

    edit login/signup logic AND style input for email/password

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -56,19 +56,33 @@ class SessionForm extends React.Component {
   }
 
   render() {
-    const userLabel = (
-      <label>
-        <input
-          className="form-submit"
-          placeholder="Username"
-          type="text"
-          value={this.state.username}
-          onChange={this.update("username")}
-          onKeyPress={this.keypress}
-          onClick={this.clickClear("username")}
-        />
-      </label>
-    );
+    const whichLabel = () => {
+      if (this.props.formType === "Signup") {
+        return (
+            <input
+              className="form-submit"
+              type="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.update("email")}
+              onKeyPress={this.keypress}
+              onClick={this.clickClear("email")}
+            />
+        )
+      } else {
+        return (
+            <input
+              className="form-submit"
+              placeholder="Email"
+              type="text"
+              value={this.state.username}
+              onChange={this.update("username")}
+              onKeyPress={this.keypress}
+              onClick={this.clickClear("username")}
+            />
+        )
+      }
+    }
 
     const passwordLabel = (
       <label>
@@ -83,29 +97,6 @@ class SessionForm extends React.Component {
         />
       </label>
     );
-
-    const emailLabel = () => {
-      if (this.props.formType === "Signup") {
-        return (
-          <div>
-            <br />
-            <label>
-              <input
-                className="form-submit"
-                type="email"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.update("email")}
-                onKeyPress={this.keypress}
-                onClick={this.clickClear("email")}
-              />
-            </label>
-          </div>
-        );
-      } else {
-        return <></>;
-      }
-    };
 
     const signupOrLogin = () => {
       if (this.props.formType === "Signup") {
@@ -125,39 +116,34 @@ class SessionForm extends React.Component {
 
     return (
       <div className="login-form-container">
-        <form onSubmit={this.handleSubmit} className="login-form-box">
+        <div className="login-header-container">
           <div onClick={this.props.closeModal} className="close-x">
             <img src="close-window-X.png"/>
           </div>
           <div className="login-header">
             <div className="login-greet">{this.props.formType}</div>
           </div>
-          <div className="render-errors">{this.renderErrors()}</div>
+        </div>
+        <form onSubmit={this.handleSubmit} className="login-form-box">
           <div className="login-form">
             <div className="label-submit">
-              {emailLabel()}
-              {userLabel}
+              <div className="email">{whichLabel()}</div>
               {passwordLabel}
             </div>
+            <div className="render-errors">{this.renderErrors()}</div>
             <input
               className="session-submit form-submit"
               type="submit"
               value={this.props.formType}
             />
-          </div>
-          <div className="demoo">
-            <button
-              className="demo-button form-submit"
-              value="Login as Demo User"
-              onClick={this.demo}
-              >
-              {" "}
-              Log In as Demo User
+            <div className="demoo">
+              <button
+                className="demo-button form-submit"
+                value="Login as Demo User"
+                onClick={this.demo}>
+                Log In as Demo User
             </button>
           </div>
-
-          <div>
-            <hr />
           </div>
           <div className="strike">{signupOrLogin()}</div>
         </form>
