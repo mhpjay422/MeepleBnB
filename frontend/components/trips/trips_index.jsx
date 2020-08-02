2b60c9d78cffe3d91738be668bcfafe217c2fc58
Jason Gong <mhpjay@gmail.com>
2020-08-02 17:16:50 -0400

    edit trip item border radius

--- frontend/components/trips/trips_index.jsx
+++ frontend/components/trips/trips_index.jsx
@@ -17,7 +17,7 @@ export default class TripsIndex extends React.Component {
         <Navbar />
         <div className="trip-container">
           <div className="trip-body">
-            <div className="trip-booked-trips-text">Your Booked Trips:</div>
+            <div className="trip-booked-trips-text">Trips</div>
             <div className="trip-content">
               <ul className="trip-items">
                 {this.props.trips.map(trip => (
