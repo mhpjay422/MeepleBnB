d45ca9e3b3e38af4d1e396e12b9b782419ca6192
Jason Gong <mhpjay@gmail.com>
2020-08-05 22:16:10 -0400

    add text to footer left and links to footer right

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,7 +6,20 @@ export default ({}) => {
     return (
       <div className="footer-container">
         <div className="footer-frame">
-          Hello
+          <div className="footer-left">
+            © 2020 Meeplebnb, Inc. All rights reserved
+          </div>
+          <div className="footer-right">
+            <a className="personal-site" href="http://jasonmgong.com/">
+              <div className="personal-div"> Personal Site</div>
+            </a>
+            <a className="linkedIn-link" href="https://www.linkedin.com/in/jason-gong-79772b126/">
+              <img className="linkedin-img" src="linkedinn.png"></img>
+            </a>
+            <a className="github-link" href="https://github.com/mhpjay422">
+              <img className="github-img" src="github.png"></img>
+            </a>
+          </div>
         </div>
       </div>
     )
