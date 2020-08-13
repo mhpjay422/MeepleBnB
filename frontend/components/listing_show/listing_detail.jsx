4cd0e8e14c4a44f3023243db8bb1eff3b2c1affc
Jason Gong <mhpjay@gmail.com>
2020-08-12 23:38:55 -0400

    add button to fullscreen google maps

--- frontend/components/listing_show/listing_detail.jsx
+++ frontend/components/listing_show/listing_detail.jsx
@@ -450,6 +450,7 @@ class ListingDetail extends React.Component {
               </div>
             </div>
             <div className="listing-addy-description">We are located in the heart of New York. Steps from local shops, bars, and restuarants. Public transit is readily available to get to wherever you would like to go around the city. The nightlife in the area is vibrant and shows why this is the city that never sleeps.</div>
+            <button title="Toggle fullscreen view" aria-label="Toggle fullscreen view" type="button"></button>
           </div>
           <div className="listing-body-location-bottom-right">
             <div className="listing-body-location-bottom-right-header">Getting Around</div>
