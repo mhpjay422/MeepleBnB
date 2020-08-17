5784f29b42e07433792ed0fea0fcfde25b22c543
Jason Gong <mhpjay@gmail.com>
2020-08-17 17:12:28 -0400

    REVIEW ACTIONS - create adjoining methods to connect to reducer

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,5 +1,46 @@
 import * as ReviewAPIUtil from "../util/review_api_util";
 
+export const RECEIVE_ALL_REVIEWS = "RECEIVE_ALL_REVIEWS";
+export const RECEIVE_REVIEW = "RECEIVE_REVIEW";
+export const RECEIVE_REVIEW_ERRORS = "RECEIVE_REVIEW_ERRORS";
+export const CLEAR_REVIEW_ERRORS = "CLEAR_REVIEW_ERRORS";
+export const DELETE_REVIEW = "DELETE_REVIEW";
+
+const receiveReviews = reviews => {
+  return {
+    type: RECEIVE_ALL_REVIEWS,
+    reviews
+  };
+};
+
+const receiveReview = review => {
+  return {
+    type: RECEIVE_REVIEW,
+    review
+  };
+};
+
+const removeReview = id => {
+  return {
+    type: DELETE_REVIEW,
+    reviewId: id
+  };
+};
+
+const receiveReviewErrors = errors => {
+  return {
+    type: RECEIVE_REVIEW_ERRORS,
+    errors
+  };
+};
+
+export const clearBookingErrors = () => {
+  return {
+    type: CLEAR_REVIEW_ERRORS
+  };
+};
+
+
 export const fetchReviews = (listingId) => dispatch => {
   return ReviewAPIUtil.fetchReviews(listingId).then(payload => {
     return dispatch(receiveReviews(payload));
