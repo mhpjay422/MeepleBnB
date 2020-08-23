1e01d4217b102d7b95141cd8413fc3b791eb4c87
Jason Gong <mhpjay@gmail.com>
2020-08-22 22:38:23 -0400

    display username and createdAt date for each review in listing detail review item

--- frontend/components/listing_show/listing_detail_review_item.jsx
+++ frontend/components/listing_show/listing_detail_review_item.jsx
@@ -4,10 +4,37 @@ import { withRouter } from "react-router-dom";
 class ListingDetailReviewItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.formatDate = this.formatDate.bind(this);
   }
 
   componentDidMount() {
     debugger
+  }
+
+  formatDate(date) {
+    const calendarYear = {
+      1: "January", 
+      "2": "February", 
+      "3": "March", 
+      "4": "April", 
+      "5": "May", 
+      "6": "June", 
+      "7": "July", 
+      "8": "August", 
+      "9": "September", 
+      "10": "October", 
+      "11": "November", 
+      "12": "December", 
+    }
+    const splitDate = date.split("-");
+    const dateYear = splitDate[0];
+    const monthDigit = parseInt(splitDate[1])
+    debugger
+    const dateMonth = calendarYear[monthDigit];
+
+    return `${dateMonth} ${dateYear}`
+    
   }
 
   render() {
@@ -20,9 +47,9 @@ class ListingDetailReviewItem extends React.Component {
               Icon
             </div>
             <div className="reviews-body-item-header-text-top">
-              Name
+              {review.author.username}
                       <div className="reviews-body-item-header-text-bottom">
-                Date
+                {this.formatDate(review.created_at)}
                       </div>
             </div>
           </div>
