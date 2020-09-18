cd407d8bce1f0000ebf5a70282dc677f0709fed7
Jason Gong <mhpjay@gmail.com>
2020-09-17 23:05:30 -0400

    edit stay options reducer to add UPDATE STAY OPTIONS as a case type

--- frontend/reducers/stay_options_reducer.js
+++ frontend/reducers/stay_options_reducer.js
@@ -1,12 +1,12 @@
 import { merge } from "lodash";
-import {
-  RECEIVE_STAY_OPTIONS
-} from "../actions/stay_options_actions";
+import { RECEIVE_STAY_OPTIONS, UPDATE_STAY_OPTIONS } from "../actions/stay_options_actions";
 
 const stayOptionsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_STAY_OPTIONS:
+      return merge({}, state, action);
+    case UPDATE_STAY_OPTIONS:
       return merge({}, state, action);
     default:
       return state;
--- frontend/util/stay_options_api_util.js
+++ frontend/util/stay_options_api_util.js
@@ -1,6 +1,4 @@
 export const fetchStayOptions = () => {
-  debugger
-  // promise?
   return Promise.resolve({})
 
   // return $.ajax({
