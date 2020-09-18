f9a61358eee5184836b964e8780bb29430245ff3
Jason Gong <mhpjay@gmail.com>
2020-09-17 23:14:18 -0400

    add dispatch of updateStayOptions to searchbarContainer

--- frontend/components/searchbar/searchbar_container.jsx
+++ frontend/components/searchbar/searchbar_container.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import SearchBar from './searchbar.jsx';
 import { fetchListings } from '../../actions/listing_actions';
 import { updateFilter } from '../../actions/filter_actions';
+import { updateStayOptions } from '../../actions/stay_options_actions';
 
 const msp = state => {
   return {
@@ -11,7 +12,8 @@ const msp = state => {
 
 const mdp = dispatch => ({
     fetchListings: (filters) => dispatch(fetchListings(filters)),
-    updateFilter: (filter, value) => dispatch(updateFilter(filter, value))
+    updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
+    updateStayOptions: (value) => dispatch(updateStayOptions(value)),
 });
 
 export default connect(msp, mdp)(SearchBar);
