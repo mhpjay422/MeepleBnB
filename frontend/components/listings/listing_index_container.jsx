bcdd4ac5eabc9ea2cc75ad77238032cb60c05f71
Jason Gong <mhpjay@gmail.com>
2020-09-17 22:22:08 -0400

    move stayOptions container logic from searchBar to listingsIndex

--- frontend/components/listings/listing_index_container.jsx
+++ frontend/components/listings/listing_index_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import {fetchListings} from '../../actions/listing_actions';
 import { fetchReviews} from '../../actions/review_actions';
+import { fetchStayOptions } from '../../actions/stay_options_actions';
 import ListingIndex from './listing_index.jsx';
 import { updateFilter } from '../../actions/filter_actions';
 import { withRouter } from "react-router-dom";
@@ -8,11 +9,13 @@ import { withRouter } from "react-router-dom";
 const msp = (state) => {
   return { 
     listings: Object.values(state.entities.listings),
-    allReviews: Object.values(state.entities.reviews)
+    allReviews: Object.values(state.entities.reviews),
+    stayOptions: Object.values(state.entities.stayOptions)
   }
 };
 
 const mdp = dispatch => ({
+  fetchStayOptions: (options) => dispatch(fetchStayOptions(options)),
   fetchListings: () => dispatch(fetchListings()),
   updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
   fetchReviews: (id) => dispatch(fetchReviews(id))
