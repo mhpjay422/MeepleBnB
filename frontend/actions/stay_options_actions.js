2c997b310e716bc2e7f52ccca4e248cd01ae546b
Jason Gong <mhpjay@gmail.com>
2020-09-17 22:51:40 -0400

    add update and change /stayOptions to stayOptionsActions. Update stay options directly dispatches value via changeStayOptions to reducer

--- frontend/actions/stay_options_actions.js
+++ frontend/actions/stay_options_actions.js
@@ -1,10 +1,20 @@
 import * as APIUtil from "../util/stay_options_api_util";
 export const RECEIVE_STAY_OPTIONS = "RECEIVE_STAY_OPTIONS";
+export const UPDATE_STAY_OPTIONS = 'UPDATE_STAY_OPTIONS';
 
 export const receiveStayOptions = options => ({
   type: RECEIVE_STAY_OPTIONS,
   options
 });
+
+export const changeStayOptions = (value) => ({
+  type: UPDATE_STAY_OPTIONS,
+  value
+});
+
+export const updateStayOptions = (value) => (dispatch) => {
+  dispatch(changeStayOptions(value));
+};
 
 export const fetchStayOptions = () => dispatch => {
   return APIUtil.fetchStayOptions().then(payload => {
