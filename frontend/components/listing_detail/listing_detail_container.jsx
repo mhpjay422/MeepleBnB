af451f41875423ee6cc25ced10d13b060efdea64
Jason Gong <mhpjay@gmail.com>
2020-09-19 16:36:38 -0400

    edit listingDetailContainer to pass stayOptions as a prop and add dispatch of fetchStayOptions in mdp

--- frontend/components/listing_detail/listing_detail_container.jsx
+++ frontend/components/listing_detail/listing_detail_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { fetchListing } from '../../actions/listing_actions';
 import ListingDetail from './listing_detail';
+import { fetchListings } from '../../actions/listing_actions';
 import {
          fetchListingReviews,
          updateReview,
@@ -11,15 +12,17 @@ import {
 const msp = (state, { match }) => {
   const listingId = parseInt(match.params.listingId);
   const listing =  state.entities.listings[listingId] || {};
-  const hostReviews = state.entities.reviews.host_reviews
-  const cloneReviews = Object.assign({}, state.entities.reviews)
-  delete cloneReviews["host_reviews"]
-  const reviews = Object.values(cloneReviews)
-  return {listing, listingId, reviews, hostReviews};
+  const hostReviews = state.entities.reviews.host_reviews;
+  const cloneReviews = Object.assign({}, state.entities.reviews);
+  delete cloneReviews["host_reviews"];
+  const reviews = Object.values(cloneReviews);
+  const stayOptions = state.entities.stayOptions
+  return { listing, listingId, reviews, hostReviews, stayOptions};
 };
 
 const mdp = dispatch => {
   return ({
+  fetchStayOptions: (options) => dispatch(fetchStayOptions(options)),
   fetchListing: id => dispatch(fetchListing(id)), 
   fetchListingReviews: id => dispatch(fetchListingReviews(id)), 
   updateReview: (id, review) => dispatch(updateReview(id, review)),
