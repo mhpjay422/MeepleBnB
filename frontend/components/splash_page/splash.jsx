e3f266e719f6eae07dbbc7e28b64ba76f4b7e43c
Jason Gong <mhpjay@gmail.com>
2020-09-23 18:06:54 -0400

    add style and content splashbuttons-button

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -1,93 +1,99 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import SearchForm from "../search_form/search_form.jsx"
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchForm from "../search_form/search_form.jsx";
 
 export default class Splash extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}; 
+    this.state = {};
 
     this.keyDownSplashLink = this.keyDownSplashLink.bind(this);
-    this.keyUpSplashLink = this.keyUpSplashLink.bind(this)
+    this.keyUpSplashLink = this.keyUpSplashLink.bind(this);
   }
 
   keyDownSplashLink() {
-    document.getElementById("splash-link").style.width =  "124px";
-    document.getElementById("splash-link").style.marginLeft =  "3px";
-    document.getElementById("splash-link").style.marginBottom =  "1px";
+    document.getElementById("splash-link").style.width = "124px";
+    document.getElementById("splash-link").style.marginLeft = "3px";
+    document.getElementById("splash-link").style.marginBottom = "1px";
     document.getElementById("splash-link").style.height = "14px";
     document.getElementById("splash-link").style.fontSize = "14px";
   }
-  
+
   keyUpSplashLink() {
     document.activeElement.blur();
-    document.getElementById("splash-link").style.marginLeft =  "0px";
+    document.getElementById("splash-link").style.marginLeft = "0px";
     document.getElementById("splash-link").style.marginBottom = "0px";
     document.getElementById("splash-link").style.width = "130px";
     document.getElementById("splash-link").style.height = "18px";
     document.getElementById("splash-link").style.fontSize = "15px";
   }
 
-
-
   render() {
-     
     const nav = (
       <div className="splash-topbar">
         <section className="topsec">
-            <div className="leftbar">
-              <Link to="/greeting" className="navbar-left">
-                <img src="./3d-meeple5.png" />
-              </Link>
-              <div className="leftbar-text">
-                meeplebnb
-              </div>
-            </div>
-            <div className="splash-search-container">
-              <div className="splash-search-container-absolute">
-                <div className="splash-search-padding">
-                  <div
-                    className="splash-search-frame"
-                    autoComplete="off"
-                  >
-                    <fieldset className="splash-search-top">
-                      <div className="splash-search-top-tabs">
-                        <div className="splash-search-top-tab">
-                          <span className="search-tab-stay">
-                            Places to stay
-                          </span>
-                        </div>
+          <div className="leftbar">
+            <Link to="/greeting" className="navbar-left">
+              <img src="./3d-meeple5.png" />
+            </Link>
+            <div className="leftbar-text">meeplebnb</div>
+          </div>
+          <div className="splash-search-container">
+            <div className="splash-search-container-absolute">
+              <div className="splash-search-padding">
+                <div className="splash-search-frame" autoComplete="off">
+                  <fieldset className="splash-search-top">
+                    <div className="splash-search-top-tabs">
+                      <div className="splash-search-top-tab">
+                        <span className="search-tab-stay">Places to stay</span>
                       </div>
-                    </fieldset>
-                    <SearchForm/>
-                  </div>
-                </div> 
+                    </div>
+                  </fieldset>
+                  <SearchForm />
+                </div>
               </div>
             </div>
-            
-            <ul className="twobar">
-              <nav className="splashbuttons">
-                <li>
-                  <button
-                    className="loginbuttons"
-                    onClick={() => this.props.openModal('login')}>Login
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="loginbuttons"
-                    onClick={() => this.props.openModal('signup')}>Signup
-                  </button>
-                </li>
-                <li>
-                    <button
-                      className="loginbuttons"
-                      value="Login as Demo User"
-                      onClick={() => this.props.demoLogin()}> Log In as Demo User
-                    </button>
-                </li>
-              </nav>
-            </ul>
+          </div>
+
+          <ul className="twobar">
+            <nav className="splashbuttons">
+              <button type="button" className="splashbuttons-button">
+                <img
+                  src="./menu-dashes.png"
+                  className="splashbuttons-menu-dashes"
+                />
+                <div className="splashbuttons-user-img">
+                  <img src="./profile.png" />
+                </div>
+              </button>
+              {/* <li>
+                <button
+                  className="loginbuttons"
+                  onClick={() => this.props.openModal("login")}
+                >
+                  Login
+                </button>
+              </li>
+              <li>
+                <button
+                  className="loginbuttons"
+                  onClick={() => this.props.openModal("signup")}
+                >
+                  Signup
+                </button>
+              </li>
+              <li>
+                <button
+                  className="loginbuttons"
+                  value="Login as Demo User"
+                  onClick={() => this.props.demoLogin()}
+                >
+                  {" "}
+                  Log In as Demo User
+                </button>
+              </li> */}
+            </nav>
+          </ul>
         </section>
       </div>
     );
@@ -98,19 +104,17 @@ export default class Splash extends React.Component {
           <div className="tagline-frame">
             <div className="tagline-padding">
               <div className="tagline-text-container">
-                <div className="tagline-main-text">
-                  Go Near or Far
-                </div>
+                <div className="tagline-main-text">Go Near or Far</div>
                 <div className="tagline-main-text-mini">
                   Travel to a destination at your leisure
                 </div>
-                <Link 
-                className="splash-link"
-                id="splash-link"
-                to='./greeting'
-                onFocus={this.keyDownSplashLink}
-                onClick={this.keyUpSplashLink}
-                onMouseOut={this.keyUpSplashLink}
+                <Link
+                  className="splash-link"
+                  id="splash-link"
+                  to="./greeting"
+                  onFocus={this.keyDownSplashLink}
+                  onClick={this.keyUpSplashLink}
+                  onMouseOut={this.keyUpSplashLink}
                 >
                   Explore New York
                 </Link>
@@ -119,18 +123,18 @@ export default class Splash extends React.Component {
           </div>
         </div>
       </>
-    )
-  
+    );
+
     const splash = () => {
-      return(
-          <>
-            <img className="splash" src="background1.jpg" aria-hidden="true"/>
-            {nav}
-            {tagline}
-          </>
+      return (
+        <>
+          <img className="splash" src="background1.jpg" aria-hidden="true" />
+          {nav}
+          {tagline}
+        </>
       );
     };
-  
+
     return splash();
-  };
+  }
 }
