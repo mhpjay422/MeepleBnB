48ccf53ebb96978fb0293491d76487d5899a4f74
Jason Gong <mhpjay@gmail.com>
2020-09-24 20:18:42 -0400

    refactor navbar and navbar container to import modal button and implement

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import SearchForm from "../search_form/search_form.jsx";
 import { convertMoment } from "../helper_methods/helper_methods.jsx";
+import ModalButton from "../modal/modal_button_container";
 
 
 class Navbar extends React.Component {
@@ -60,22 +61,22 @@ class Navbar extends React.Component {
   }
 
   toggleSearch() {
-    if(this.state.searchBarType === "mini") {
-      this.setState({ searchBarType: "full"})
+    if (this.state.searchBarType === "mini") {
+      this.setState({ searchBarType: "full" })
     } else {
-      this.setState({ searchBarType: "mini"})
+      this.setState({ searchBarType: "mini" })
     }
   }
 
   setDataFromSearchForm(search, start, end, numGuests) {
     this.setState({
       searchTerm: search,
-      startDate: start, 
-      endDate: end, 
+      startDate: start,
+      endDate: end,
       guests: numGuests
     })
   }
-  
+
   propSearchTerm() {
     return this.props.stayOptions.searchTerm
   }
@@ -93,213 +94,184 @@ class Navbar extends React.Component {
   }
 
   render() {
-  
-  const demo = () => {
-    if (this.props.loggedIn) {
-      return (
-        <div className="loggedin-buttons" >
-          <Link className="header-button" to={`/bookings/${this.props.users.id}`}>
-            <button className="header-button">Trips</button>
-          </Link>
-          <Link className="header-button" onClick={this.logOut} to={`/greeting`}>
-            <button className="header-button">Log Out</button>
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <nav className="greet-splashbuttons">
-          <li>
-            <button
-              className="greet-loginbuttons"
-              onClick={() => this.props.openModal("login")}
-            >
-              Login
-            </button>
-          </li>
-          <li>
-            <button
-              className="greet-loginbuttons"
-              onClick={() => this.props.openModal("signup")}
-            >
-              Signup
-            </button>
-          </li>
-          <li>
-            <Link to={`/greeting`}>
-              <button
-                className="greet-loginbuttons"
-                value="Login as Demo User"
-                onClick={this.logInDemo}
-              >
-                {" "}
-                LogIn as Demo User
-              </button>
+
+    const demo = () => {
+      if (this.props.loggedIn) {
+        return (
+          <div className="loggedin-buttons" >
+            <Link className="header-button" to={`/bookings/${this.props.users.id}`}>
+              <button className="header-button">Trips</button>
             </Link>
-          </li>
-        </nav>
-      );
-    }
-  };
-
-  const miniOrFull = () => {
-    if(this.state.searchBarType === "mini") { 
-      return "mini"
-    } else {
-      return "full"
-    }
-  }
-
-  const searchLocation = () => {
-    if (this.propSearchTerm()) {
-      return this.propSearchTerm()
-    } else {
-      return "Add Location"
-    }
-  }
-
-  const searchDates = () => {
-
-    if (convertMoment(this.propStartDate(), this.propEndDate())) {
-      return convertMoment(this.propStartDate(), this.propEndDate())
-    } else {
-      return "Add Dates"
-    }     
-  }
-
-  const searchGuests = () => {
-    if (this.propGuests()) {
-      if(this.propGuests() === 1) {
-
-        return (
-          `${this.propGuests()} guest`
-        )
-      } else {
-
-        return (
-          `${this.propGuests()} guests`
-        )
-      }
-
-    } else {
-      return "Add Guests"
-    }
-  }
-
-
-
-  const greetSearchMini = () => {
-    return (
-      <div 
-      className={`greet-search-mini-container-${miniOrFull()}`}
-      onClick={this.toggleSearch}
-      >
-        <div className="greet-search-mini-frame">
-          <button className="greet-search-mini-item">
-            <div className="greet-search-mini-text">
-              {searchLocation()}
-            </div>
-          </button>
-          <div className="greet-search-mini-divider" />
-          <button className="greet-search-mini-item">
-            <div className="greet-search-mini-text">
-              {searchDates()}
-            </div>
-          </button>
-          <div className="greet-search-mini-divider" />
-          <button className="greet-search-mini-item">
-            <div className="greet-search-mini-text">
-              {searchGuests()}
-                  </div>
-            <div className="greet-search-mini-submit">
-              <div className="greet-search-mini-submit-image">
-                <img className="greet-search-mini-submit-img" src="magsm.png"></img>
-              </div>
-            </div>
-          </button>
-        </div>
-      </div>
-    )
-  }
-
-  const greetSearchFull = () => {
-    return (
-      <div className={`greet-search-full-container-${miniOrFull()}`}>
-        <div className="splash-search-padding">
-          <div
-            className="splash-search-frame"
-            autoComplete="off"
-          >
-            <fieldset className="splash-search-top">
-              <div className="splash-search-top-tabs">
-                <div className="splash-search-top-tab">
-                  <span className="search-tab-stay-index">
-                    Places to stay
-                  </span>
-                </div>
-              </div>
-            </fieldset>
-            <SearchForm 
-            toggleSearch={this.toggleSearch}
-            setDataFromSearchForm={this.setDataFromSearchForm}
-            />
+            <Link className="header-button" onClick={this.logOut} to={`/greeting`}>
+              <button className="header-button">Log Out</button>
+            </Link>
           </div>
-        </div>
-      </div>
-    )
-  }
+        );
+      } else {
+        return (
+          <ModalButton />
+        );
+      }
+    };
 
-  const greetGrayout = () => {
-    if(this.state.searchBarType === "full") {
+    const miniOrFull = () => {
+      if (this.state.searchBarType === "mini") {
+        return "mini"
+      } else {
+        return "full"
+      }
+    }
+
+    const searchLocation = () => {
+      if (this.propSearchTerm()) {
+        return this.propSearchTerm()
+      } else {
+        return "Add Location"
+      }
+    }
+
+    const searchDates = () => {
+
+      if (convertMoment(this.propStartDate(), this.propEndDate())) {
+        return convertMoment(this.propStartDate(), this.propEndDate())
+      } else {
+        return "Add Dates"
+      }
+    }
+
+    const searchGuests = () => {
+      if (this.propGuests()) {
+        if (this.propGuests() === 1) {
+
+          return (
+            `${this.propGuests()} guest`
+          )
+        } else {
+
+          return (
+            `${this.propGuests()} guests`
+          )
+        }
+
+      } else {
+        return "Add Guests"
+      }
+    }
+
+
+
+    const greetSearchMini = () => {
       return (
         <div
-          className="greet-screen-grayout"
+          className={`greet-search-mini-container-${miniOrFull()}`}
           onClick={this.toggleSearch}
-        />
-      )
-    } else {
-      return <></>
-    }
-  }
-
-  const topbar = () => {
-    if(this.state.searchBarType === "mini") {
-      return "topbar"
-    } else {
-      return "topbar-full"
-    }
-  }
-
-  const navbar = () => {
-    return (
-      <div className="navbarZ">
-        <div className={topbar()}>
-          <section className="greet-topsec">
-            <section className="greet-leftsec">
-              <div className="greet-logo">
-                <img src="./3d-meepleneg.png" className="navbar-left" onClick={this.handleClick} />
+        >
+          <div className="greet-search-mini-frame">
+            <button className="greet-search-mini-item">
+              <div className="greet-search-mini-text">
+                {searchLocation()}
               </div>
-              <div className="greetbar-text">
-                <div>meeplebnb</div>
+            </button>
+            <div className="greet-search-mini-divider" />
+            <button className="greet-search-mini-item">
+              <div className="greet-search-mini-text">
+                {searchDates()}
               </div>
-            </section>
-            <div className="greet-search-container">
-              {greetSearchMini()}
-              {greetSearchFull()}
-            </div>
-            <div className="twobar">{demo()}</div>
-          </section>
+            </button>
+            <div className="greet-search-mini-divider" />
+            <button className="greet-search-mini-item">
+              <div className="greet-search-mini-text">
+                {searchGuests()}
+              </div>
+              <div className="greet-search-mini-submit">
+                <div className="greet-search-mini-submit-image">
+                  <img className="greet-search-mini-submit-img" src="magsm.png"></img>
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
-        {greetGrayout()}
-      </div>
-    )
-  }
+      )
+    }
 
-  const linkToSessions = () => {
-    return <nav>{navbar()}</nav>;
-  };
+    const greetSearchFull = () => {
+      return (
+        <div className={`greet-search-full-container-${miniOrFull()}`}>
+          <div className="splash-search-padding">
+            <div
+              className="splash-search-frame"
+              autoComplete="off"
+            >
+              <fieldset className="splash-search-top">
+                <div className="splash-search-top-tabs">
+                  <div className="splash-search-top-tab">
+                    <span className="search-tab-stay-index">
+                      Places to stay
+                  </span>
+                  </div>
+                </div>
+              </fieldset>
+              <SearchForm
+                toggleSearch={this.toggleSearch}
+                setDataFromSearchForm={this.setDataFromSearchForm}
+              />
+            </div>
+          </div>
+        </div>
+      )
+    }
 
-  return linkToSessions();
+    const greetGrayout = () => {
+      if (this.state.searchBarType === "full") {
+        return (
+          <div
+            className="greet-screen-grayout"
+            onClick={this.toggleSearch}
+          />
+        )
+      } else {
+        return <></>
+      }
+    }
+
+    const topbar = () => {
+      if (this.state.searchBarType === "mini") {
+        return "topbar"
+      } else {
+        return "topbar-full"
+      }
+    }
+
+    const navbar = () => {
+      return (
+        <div className="navbarZ">
+          <div className={topbar()}>
+            <section className="greet-topsec">
+              <section className="greet-leftsec">
+                <div className="greet-logo">
+                  <img src="./3d-meepleneg.png" className="navbar-left" onClick={this.handleClick} />
+                </div>
+                <div className="greetbar-text">
+                  <div>meeplebnb</div>
+                </div>
+              </section>
+              <div className="greet-search-container">
+                {greetSearchMini()}
+                {greetSearchFull()}
+              </div>
+              <div className="twobar">{demo()}</div>
+            </section>
+          </div>
+          {greetGrayout()}
+        </div>
+      )
+    }
+
+    const linkToSessions = () => {
+      return <nav>{navbar()}</nav>;
+    };
+
+    return linkToSessions();
 
   };
 }
--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import { login, logout, demoLogin } from '../../actions/session_actions';
 import Navbar from './navbar';
-import { openModal } from '../../actions/modal_actions';
 
 const msp = (state) => {
 
@@ -12,16 +11,9 @@ const msp = (state) => {
   };
 };
 
-const mdp  = dispatch => {
+const mdp = dispatch => {
   return {
-    login: () => dispatch(login()),
     logout: () => dispatch(logout()),
-    openModal: modal => dispatch(openModal(modal)),
-    demoLogin: () => dispatch(demoLogin(
-      {user: {username: "demoUser",
-      email: "demoUser@gmail.com",
-      password: "starwars"}}
-    ))
   };
 };
 
