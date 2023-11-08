d654711c4439035ab48dcaab8e8540afff9b53f8
Jason Gong <jasongong83@gmail.com>
2023-11-07 19:24:04 -0500

    remove personal

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 
 export default ({}) => {
-
   const footer = () => {
     return (
-        <>  
-          <div className="footer-left">
-            © 2020 Meeplebnb, Inc. All rights reserved
-          </div>
-          <div className="footer-right">
-            <a className="personal-site" href="http://jasonmgong.com/">
-              <div className="personal-div"> Personal Site</div>
-            </a>
-            <a className="linkedIn-link" href="https://www.linkedin.com/in/jason-gong-79772b126/">
-              <img className="linkedin-img" src="linked.png"></img>
-            </a>
-            <a className="github-link" href="https://github.com/mhpjay422">
-              <img className="github-img" src="github2.png"></img>
-            </a>
-          </div>
-        </> 
-    )
-  }
+      <>
+        <div className="footer-left">
+          © 2020 Meeplebnb, Inc. All rights reserved
+        </div>
+        <div className="footer-right">
+          <a
+            className="linkedIn-link"
+            href="https://www.linkedin.com/in/jason-gong-79772b126/"
+          >
+            <img className="linkedin-img" src="linked.png"></img>
+          </a>
+          <a className="github-link" href="https://github.com/mhpjay422">
+            <img className="github-img" src="github2.png"></img>
+          </a>
+        </div>
+      </>
+    );
+  };
 
   return footer();
-}
+};
